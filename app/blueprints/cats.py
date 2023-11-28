295b52db12e2ce3ed01f84db0b7a0c01569ed5c0
KelvinQiu <kelvinqiu802@outlook.com>
2023-11-28 13:59:12 +0800

    feat: cats api

--- app/blueprints/cats.py
+++ app/blueprints/cats.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify, Response
 from models.cats import Cats
 from extentions import db
 
+
 cats = Blueprint('cats', __name__)
 
 
@@ -16,3 +17,18 @@ Returns:
 def all_cats() -> Response:
     all_cats: list[Cats] = Cats.query.all()
     return jsonify([cat.to_dict() for cat in all_cats])
+
+
+@cats.post('/')
+def create_cat() -> Response:
+    new_cat = Cats(**request.json)
+    db.session.add(new_cat)
+    db.session.commit()
+    return Response(status=201, response=f'New Cats {request.json["name"]} Created')
+
+
+@cats.delete('/')
+def remove_all() -> Response:
+    db.session.query(Cats).delete()
+    db.session.commit()
+    return Response(status=204, response="All Cats Removed")
